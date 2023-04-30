71ab32e0dfd3fa26d0caa95db4740d369804026a
Anastasiia Pastukhova <nastapastuhova778@gmail.com>
2023-04-30 22:15:27 +0300

    changes

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,31 @@
+import { FriendListItem } from './FriendListItem/FriendListItem';
+import PropTypes from 'prop-types';
+import css from './FriendList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ avatar, name, isOnline, id }) => {
+        return (
+          <FriendListItem
+            key={id}
+            avatar={avatar}
+            name={name}
+            isOnline={isOnline}
+          />
+        );
+      })}
+    </ul>
+  );
+};
+
+FriendList.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      avatar: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
